2ecd3f47e3dd2b75f091faf0526c856380a9fc2d
unknown <meliodas_17@hotmail.com>
2022-11-29 11:04:52 -0300

    modifing LINK_DB to

--- api/src/db.js
+++ api/src/db.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
 
-const connectionString = LINK_DB;
+const connectionString = process.env.LINK_DB;
 
 // mongoose.connect(connectionsString,{
 //     useNewUrlParser: true,
